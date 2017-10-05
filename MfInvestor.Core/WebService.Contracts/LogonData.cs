1b8f4ece459760935b9840fde0fb1f5d7686828f
dhayanithims <dhayanithims@users.noreply.github.com>
2017-10-05 23:30:58 +0530

    Pushing contracts project

---
+++ MfInvestor.Core/WebService.Contracts/LogonData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebService.Contracts
+{
+    public class LogonData : IIdentity
+    {
+        public string UserName { get; set; }
+        public long Id { get; set; }
+        public string PwdSalt { get; set; }
+        public string Password { get; set; }
+        public DateTime LastLogin { get; set; }
+        public DateTime LogoutTime { get; set; }
+        public bool IsActive { get; set; }
+        public int FailedAttempts { get; set; }
+        public DateTime LastAttempt { get; set; }        
+    } 
+    
+}
---
+++ MfInvestor.Core/WebService.Contracts/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WebService.Contracts
+{
+    public class User : IIdentity {
+
+        public string FirstName { get; set; }
+        public long Id { get ; set ; }
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+        public string MobileNumber { get; set; }
+    } 
+
+    public class UserRegistration
+    {
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+        public string MobileNumber { get; set; }
+    }
+
+    public interface IIdentity
+    {
+        long Id { get; set; }
+    }
+}
